2aae93e446af502fac3c6d6196dcddb54b59a2b8
danielsteman <daniel-steman@live.nl>
2022-08-01 21:49:34 +0200

    :sparkles: Added brewfile

---
+++ Brewfile
@@ -0,0 +1,14 @@
+# shell tools
+brew "tmux"
+brew "zsh"
+brew "zsh-syntax-highlighting"
+
+# vim tools
+brew "neovim"
+
+# languages
+brew "node"
+brew "pyenv"
+brew "pyenv-virtualenv"
+brew "rustup"
+
--- copy_files.sh
+++
@@ -1,9 +0,0 @@
-if test -f "~/.zshrc"; then
-    read -p "Are you sure you want to overwrite .zshrc and .vimrc? " -n 1 -r
-    echo 
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        rm ~/.zshrc
-        ln -s zsh/.zshrc ~
-    fi
-
