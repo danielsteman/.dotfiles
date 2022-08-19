12ed777590efab6fbde0d6be8b9cebecbaa9797f
Daniel Steman <daniel-steman@live.nl>
2022-08-19 11:37:38 +0000

    added automation to setup terminal

---
+++ create-terminal-setup.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/nvim/ ~/.config/nvim
+cp ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
