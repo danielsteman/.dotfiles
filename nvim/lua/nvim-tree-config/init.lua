b3969abbfdd6243ad873bd3465658917755e0927
danielsteman <daniel-steman@live.nl>
2022-09-20 21:12:07 +0200

    :sparkles: neovim upgrades

--- nvim/lua/nvim-tree-config/init.lua
+++ nvim/lua/nvim-tree-config/init.lua
@@ -1,2 +1,3 @@
 require'nvim-tree'.setup {
+  sort_by = "case_sensitive"
 }
--- nvim/lua/packer-config/init.lua
+++ nvim/lua/packer-config/init.lua
@@ -9,5 +9,10 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+  use 'neovim/nvim-lspconfig' 
 end)
   
