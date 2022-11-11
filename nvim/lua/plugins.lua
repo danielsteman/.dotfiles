d71056209b220ecc73a803ae53b5ae6bc7870b62
Daniel Steman <daniel-steman@live.nl>
2022-11-11 17:55:15 +0100

    lsp wip

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,14 +3,14 @@ local Plugin = {}
 function Plugin.setup()
   local function plugins(use)
 
-    require("utils.settings").setup()
-    require("config.keymap").setup()
+    require('utils.settings').setup()
+    require('config.keymap').setup()
 
     use { 'wbthomason/packer.nvim' }
     use {
       'kyazdani42/nvim-web-devicons',
       config = function()
-        require("nvim-web-devicons").setup { default = true }
+        require('nvim-web-devicons').setup { default = true }
       end,
     }
     use {
@@ -25,10 +25,22 @@ function Plugin.setup()
         require('config.theme').setup()
       end,
     }
+    use {
+      'neovim/nvim-lspconfig',
+      opt = true,
+      event = 'BufReadPre',
+      wants = { 'nvim-lsp-installer' },
+      config = function()
+        require('config.lsp').setup()
+      end,
+      requires = {
+        'williamboman/nvim-lsp-installer',
+      },
+    }
 
   end
 
-  require("packer").startup(plugins)
+  require('packer').startup(plugins)
 
 end
 
