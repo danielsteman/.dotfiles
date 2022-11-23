27f5bec0bf97d8ad65e19071ab4f786bf338d125
Daniel Steman <daniel-steman@live.nl>
2022-11-23 10:36:42 +0000

    :construction: disable lsp temporarily

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -25,18 +25,18 @@ function Plugin.setup()
         require('config.theme').setup()
       end,
     }
-    use {
-      'neovim/nvim-lspconfig',
-      opt = true,
-      event = 'BufReadPre',
-      wants = { 'nvim-lsp-installer' },
-      config = function()
-        require('config.lsp').setup()
-      end,
-      requires = {
-        'williamboman/nvim-lsp-installer',
-      },
-    }
+    -- use {
+    --   'neovim/nvim-lspconfig',
+    --   opt = true,
+    --   event = 'BufReadPre',
+    --   wants = { 'nvim-lsp-installer' },
+    --   config = function()
+    --     require('config.lsp').setup()
+    --   end,
+    --   requires = {
+    --     'williamboman/nvim-lsp-installer',
+    --   },
+    -- }
 
   end
 
