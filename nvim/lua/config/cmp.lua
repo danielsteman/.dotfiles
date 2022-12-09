73e1762b5a95900173e349c5387b602819566325
Daniel Steman <daniel-steman@live.nl>
2022-12-09 18:00:18 +0100

    installed lsp and lsp_signature

---
+++ nvim/lua/config/cmp.lua
@@ -0,0 +1,12 @@
+sources = {
+  { name = "nvim_lsp" },
+  { name = "treesitter" },
+  { name = "buffer" },
+  { name = "luasnip" },
+  { name = "nvim_lua" },
+  { name = "path" },
+},
+documentation = {
+  border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
+},
