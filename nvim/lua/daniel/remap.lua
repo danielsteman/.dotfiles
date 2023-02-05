62e519f3eb4fabb71117274fec0eff9ce2ea6fb8
Daniel Steman <daniel-steman@live.nl>
2023-02-05 19:06:12 +0100

    updated keymap to paste from clipboard

--- nvim/lua/daniel/remap.lua
+++ nvim/lua/daniel/remap.lua
@@ -6,3 +6,9 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+-- remap to paste from clipboard
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>P", "\"+P")
+vim.keymap.set("v", "<leader>P", "\"+P")
+
