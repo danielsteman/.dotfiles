2e7896900754153b6a31c82ae9eb0bb4acb94705
danielsteman <daniel-steman@live.nl>
2023-04-05 18:38:19 +0700

    :art: auto formatting

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,11 +2,11 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'lua_ls',
-	'rust_analyzer',
-	'pyright',
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'rust_analyzer',
+    'pyright',
 })
 
 lsp.configure('lua_ls', {
@@ -20,32 +20,32 @@ lsp.configure('lua_ls', {
 })
 
 lsp.format_on_save({
-  servers = {
-    ['lua_ls'] = {'lua'},
-    ['rust_analyzer'] = {'rust'},
-    ['tsserver'] = {'ts', 'tsx'},
-    ['pyright'] = {'py'},
-  }
+    servers = {
+        ['lua_ls'] = { 'lua' },
+        ['rust_analyzer'] = { 'rust' },
+        ['tsserver'] = { 'ts', 'tsx' },
+        ['black'] = { 'py' },
+    }
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
-lsp.on_attach(function (client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 
 end)
 
