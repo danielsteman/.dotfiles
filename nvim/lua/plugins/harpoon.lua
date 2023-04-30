dd0e5ab4db75ad8f2d54249160c92ac866b58430
danielsteman <daniel-steman@live.nl>
2023-04-30 22:32:44 +0200

    :bug: fix

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -11,5 +11,5 @@ return {
       vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
       vim.keymap.set("n", "<C-d>", function() ui.nav_file(3) end)
       vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-    end 
+    end
 }
