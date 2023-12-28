d7145fc94c3f2aeae8be41c5cc159a9a203037a1
Daniel Steman <daniel-steman@live.nl>
2023-12-28 14:41:42 +0100

    added global gitignore

--- unix/create-terminal-setup.sh
+++ unix/create-terminal-setup.sh
@@ -3,5 +3,7 @@ ln -sf ~/.dotfiles/nvim ~/.config/nvim
 yes | cp -rf ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 yes | cp -rf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k || (cd ~/powerlevel10k ; git pull)
-
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k || (
+  cd ~/powerlevel10k
+  git pull
+)
