7bb9a7e7fb7a3807da0c26488c004bb8e6533eff
Daniel Steman <daniel-steman@live.nl>
2024-03-07 22:09:00 +0100

    update sway stuff

--- unix/asahi-fedora/sway/bar.sh
+++ unix/asahi-fedora/sway/bar.sh
@@ -1,10 +1,12 @@
 while true
 do
     # $(cmd) stores the output of cmd
-    date=$(date +'%A, %b %d')
-    time=$(date +'%I%M %p')
+    date=$(date +'%a, %b %d, %Y')
+    time=$(date +'%T')
+    volume=$(pamixer --get-volume-human)
+    brightness=$(brightnessctl g)
 
     # => Friday, Mar 05 | 03:47 PM
-    echo "$date | $time"
+    echo "$date | $time | 🔊 $volume | 💡 $brightness"
     sleep 1
-done 
+done
