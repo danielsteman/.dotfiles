f552c23c9f743f5ec2debecdceb8a3dc96875436
Daniel Steman <daniel-steman@live.nl>
2024-10-01 22:33:50 +0200

    change background color

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,10 +4,14 @@ local config = {}
 config.color_scheme = "Catppuccin Mocha"
 config.tab_bar_at_bottom = true
 
-config.font = wezterm.font 'MesloLGS NF'
+config.font = wezterm.font('MesloLGS NF')
 config.font_size = 16.0
 
 config.window_background_opacity = 0.6
 config.macos_window_background_blur = 20
+
+config.colors = {
+    background = "#000000", 
+}
 
 return config
