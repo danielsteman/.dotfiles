e639eac1eab6d0e4a7e23f3a3cb14aa79d1b227e
Daniel Steman <daniel-steman@live.nl>
2024-10-02 12:48:01 +0200

    add key bindings for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+-- Load keys config
+local cfg_keys = require 'keys'
+config.keys = cfg_keys.keys
+
 config.color_scheme = "Catppuccin Mocha"
 config.tab_bar_at_bottom = true
 
