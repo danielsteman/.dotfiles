af092c5c1e5b79c8226af1860f03636568a36640
Daniel Steman <daniel-steman@live.nl>
2024-10-09 11:39:36 +0200

    change color scheme to catpuccin

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,21 +1,13 @@
--- function ColorMyPencils(color)
--- 	color = color or "rose-pine"
--- 	vim.cmd.colorscheme(color)
-
--- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- end
-
--- ColorMyPencils()
-
 return {
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-				vim.cmd('colorscheme rose-pine')
-				color = color or "rose-pine"
-				vim.cmd.colorscheme(color)
-				vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-				vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end
+    {
+        "catppuccin/nvim",
+        name = "catppuccin", 
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end
+    }
 }
