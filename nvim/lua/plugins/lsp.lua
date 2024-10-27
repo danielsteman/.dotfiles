0c134eb40527078b7eff2e6f8c40a21fa321783c
Daniel Steman <daniel-steman@live.nl>
2024-10-27 21:51:26 +0100

    format async

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -56,17 +56,19 @@ return {
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					buffer = bufnr,
 					callback = function()
-						vim.lsp.buf.format({ bufnr = bufnr })
+						vim.lsp.buf.format({ async = true, bufnr = bufnr })
 					end,
 				})
 			end
 		end)
 
 		local null_ls = require("null-ls")
+
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.black.with({
 					extra_args = { "--fast" },
+					filetypes = { "python" },
 				}),
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.stylua,
