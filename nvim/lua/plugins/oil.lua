d59853538a1af2492ce633ee2e31232a2ab160c2
Daniel Steman <daniel-steman@live.nl>
2024-10-30 11:59:31 +0100

    disable default oil keymaps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,9 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
+	opts = {
+		use_default_keymaps = false,
+	},
 	-- Optional dependencies
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
