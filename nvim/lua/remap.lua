771b2aa06114f6bc75532c36a6a83e124f0e7f79
Daniel Steman <daniel-steman@live.nl>
2024-10-30 11:38:02 +0100

    add oil nvim

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,15 +1,15 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- remap to copy to clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
 
 -- remap to paste from clipboard
-vim.keymap.set("n", "<leader>p", "\"+p")
-vim.keymap.set("v", "<leader>p", "\"+p")
-vim.keymap.set("n", "<leader>P", "\"+P")
-vim.keymap.set("v", "<leader>P", "\"+P")
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>P", '"+P')
+vim.keymap.set("v", "<leader>P", '"+P')
 
 -- remap to run python script
-vim.keymap.set('n', '<F4>', ":w <bar> exec '!python3 '.shellescape('%')<CR>")
+vim.keymap.set("n", "<F4>", ":w <bar> exec '!python3 '.shellescape('%')<CR>")
