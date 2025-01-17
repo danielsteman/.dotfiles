8c6f94741c07d3fd69024fb1012e14a074ba39ef
Daniel Steman <daniel-steman@live.nl>
2025-01-17 11:52:59 +0100

    add scroll 15 lines up down remap

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -13,3 +13,7 @@ vim.keymap.set("v", "<leader>P", '"+P')
 
 -- remap to run python script
 vim.keymap.set("n", "<F4>", ":w <bar> exec '!python3 '.shellescape('%')<CR>")
+
+-- remap to scroll 15 lines down and up
+vim.keymap.set("n", "<C-j>", "15j", { desc = "Scroll down 15 lines" })
+vim.keymap.set("n", "<C-k>", "15k", { desc = "Scroll up 15 lines" })
