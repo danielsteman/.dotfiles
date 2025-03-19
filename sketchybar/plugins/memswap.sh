311a9424fe4dc92550321ac44297d253610ee2f0
Daniel Steman <daniel-steman@live.nl>
2025-03-19 20:11:44 +0100

    fix memory bash script

--- sketchybar/plugins/memswap.sh
+++ sketchybar/plugins/memswap.sh
@@ -2,13 +2,19 @@
 
 DEFCOLOR="0x44FFFFFF"
 ALERTCOLOR="0xAAFF0000"
-TOTALSWAP="$(sysctl vm.swapusage | awk '{print $4}')"
+MEM_USED=$(vm_stat | awk -F ':[ ]*' '
+  /Pages active/ {active=$2}
+  /Pages wired/ {wired=$2}
+  /Pages occupied by compressor/ {compressed=$2}
+  END {printf "%.2f GB", (active + wired + compressed) * 4096 / 1024^3}')
 
-clr=""
-if [ "$TOTALSWAP" != "0.00M" ]; then
+clr="$DEFCOLOR"
+CRITICAL_THRESHOLD=14
+
+MEM_VALUE=$(echo "$MEM_USED" | awk '{print $1}')
+
+if [ "$(echo "$MEM_VALUE > $CRITICAL_THRESHOLD" | bc -l)" -eq 1 ]; then
     clr="$ALERTCOLOR"
-else
-    clr="$DEFCOLOR"
 fi
 
-sketchybar --set "$NAME" label="$TOTALSWAP" icon.color="$clr" label.color="$clr"
+sketchybar --set "$NAME" label="$MEM_USED" icon.color="$clr" label.color="$clr"
