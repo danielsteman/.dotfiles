ecc3423d498adb2fe4ac8bdd2c6ad1cb19397c2b
Daniel Steman <daniel-steman@live.nl>
2025-03-23 22:00:35 +0100

    add path to nvim tab

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -22,3 +22,8 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.opt.clipboard = "unnamed,unnamedplus"
+
+vim.opt.title = true
+vim.opt.titlelen = 0
+vim.opt.titlestring = 'v %{expand("%:p")}'
+
