6fdf0b824ab894f09342bbf1fbac7884ccfae17a
Daniel Steman <daniel-steman@live.nl>
2025-06-11 10:30:41 +0200

    add home manager

--- nix-darwin-config/flake.nix
+++ nix-darwin-config/flake.nix
@@ -1,13 +1,18 @@
 {
   description = "Example nix-darwin system flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       nixpkgs.config.allowUnfree = true;
