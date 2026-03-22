aa6509d4160263ca40453d615a107a1265d7faf9
Daniel Steman <daniel-steman@live.nl>
2026-03-22 16:45:13 +0100

    update nix config

--- nix-darwin-config/home.nix
+++ nix-darwin-config/home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "danielsteman";
-  # home.homeDirectory is set in flake.nix
+  home.homeDirectory = "/Users/danielsteman";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
